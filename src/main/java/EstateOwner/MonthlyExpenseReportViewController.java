475d202c2e59981dc24bea6c12cb02b6c5412484
Tech Solution <shahriarrahat2002@gmail.com>
2026-07-25 00:08:20 +0600

    FXML Design and Controller Update

---
+++ src/main/java/EstateOwner/MonthlyExpenseReportViewController.java
@@ -0,0 +1,49 @@
+package EstateOwner;
+
+public class MonthlyExpenseReportViewController
+{
+    @javafx.fxml.FXML
+    private TableColumn colAmount;
+    @javafx.fxml.FXML
+    private Button btnBack;
+    @javafx.fxml.FXML
+    private Button btnLoadExpenses;
+    @javafx.fxml.FXML
+    private Button btnRefresh;
+    @javafx.fxml.FXML
+    private Button btnExport;
+    @javafx.fxml.FXML
+    private TableView expenseTable;
+    @javafx.fxml.FXML
+    private TableColumn colDescription;
+    @javafx.fxml.FXML
+    private TableColumn colDate;
+    @javafx.fxml.FXML
+    private TableColumn colCategory;
+    @javafx.fxml.FXML
+    private ComboBox cmbYear;
+    @javafx.fxml.FXML
+    private Label lblTotalExpense;
+    @javafx.fxml.FXML
+    private ComboBox cmbMonth;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void goBack(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void loadExpenseReport(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void exportReport(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void refreshReport(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/gardenManager/IrrigationActivityViewController.java
@@ -0,0 +1,43 @@
+package gardenManager;
+
+public class IrrigationActivityViewController
+{
+    @javafx.fxml.FXML
+    private Button btnBack;
+    @javafx.fxml.FXML
+    private Button btnSave;
+    @javafx.fxml.FXML
+    private DatePicker dpActivityDate;
+    @javafx.fxml.FXML
+    private TextField txtEndTime;
+    @javafx.fxml.FXML
+    private Label lblTotalWater;
+    @javafx.fxml.FXML
+    private TextArea txtRemarks;
+    @javafx.fxml.FXML
+    private ComboBox cmbZone;
+    @javafx.fxml.FXML
+    private TextField txtWaterUsed;
+    @javafx.fxml.FXML
+    private Button btnClear;
+    @javafx.fxml.FXML
+    private Label lblStatus;
+    @javafx.fxml.FXML
+    private TextField txtStartTime;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void goBack(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void saveIrrigationRecord(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void clearForm(ActionEvent actionEvent) {
+    }
+}
