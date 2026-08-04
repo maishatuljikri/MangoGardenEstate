214cfc4c5ab427a56d90424acfb2c5262300264b
fahim-007-ai <fahimshawon2020@gamil.com>
2026-08-05 03:07:29 +0600

    update shawon 2 user

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,4 +9,9 @@ module com.example.mangogardenestate {
     opens com.example.mangogardenestate.EstateOwner to javafx.fxml;
     exports com.example.mangogardenestate.gardenManager;
     opens com.example.mangogardenestate.gardenManager to javafx.fxml;
+    exports com.example.mangogardenestate.SecurityGuard;
+    opens com.example.mangogardenestate.SecurityGuard to javafx.fxml;
+    exports com.example.mangogardenestate.PackagingOfficer;
+    opens com.example.mangogardenestate.PackagingOfficer to javafx.fxml;
+    opens com.example.mangogardenestate.common to javafx.fxml;
 }
